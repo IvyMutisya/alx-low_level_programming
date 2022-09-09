26c8a123d5fe795274ee9ea70c58492f97e92140
IvyMutisya <ivymutisya97@gmail.com>
2022-09-08 22:52:17 -0700

    printing using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+
+ *main entry point
+
+ *Return:set always 0 (success)
+
  */
+
 int main(void)
   
-{ puts("\"Programming is like building a multilingual puzzle"); return (0); 
+{ puts ("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
 }
