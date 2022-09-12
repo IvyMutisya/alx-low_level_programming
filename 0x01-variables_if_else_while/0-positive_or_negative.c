af6c6131a8193a04e91a63de2d9ca7826266f098
IvyMutisya <ivymutisya97@gmail.com>
2022-09-12 00:38:11 -0700

    determining if the numbers arepositive, negative or equal to zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,15 +5,24 @@
 /* determining whether the numbers are positive or negative
  *if the number > 0 then it is positive
  *if the number is < 0 then it is negative
- if the number is 0 then it is 0 */
+ if the number is 0 then it is 0 
+*/
 int main(void)
-{ int n;
+{
+int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n>0)
-{ printf ("%d is positive \n",n); }
+{
+printf ("%d is positive \n",n);
+}
 else if (n == 0)
-{ printf ("%d is zero \n", n); }
+{
+printf ("%d is zero \n", n);
+}
 else
-{ printf ("%d is negative \n", n) ;}
-return (0); }
+{
+printf ("%d is negative \n", n);
+}
+return (0);
+}
