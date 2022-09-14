4ba170c753be7e0ab80667326f59ca6615cd34bd
IvyMutisya <ivymutisya97@gmail.com>
2022-09-14 00:37:28 -0700

    working with putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 /**
-* main - Entry point
+* main - Prints Holberton as a message.
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-#include 'main.h'
-char *sh = "Putchar";
-while (*sh)
+int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+int count, sz;
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz; count++)
 {
-_putchar(*sh);
-sh++;
+_putchar(str[count]);
 }
 _putchar('\n');
 return (0);
 }
+
